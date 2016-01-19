7932c5a080d3b79cd98dd3bc7ccc4b86d6e58d56
sapake <codinghobo@gmail.com>
2016-01-19 17:04:40 +0200

    Initial commit :)

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace oliot_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numStrings = { "one", "two", "three" };
+            int input = Int32.Parse(Console.ReadLine()) - 1;
+            if(input < numStrings.Length)
+            {
+                Console.WriteLine(numStrings[input]);
+            } else {
+                Console.WriteLine("some other number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
