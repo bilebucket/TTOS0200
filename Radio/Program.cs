b0be6c1badd0588cba4cecbca5d9e3c81a47e5e7
sapake <codinghobo@gmail.com>
2016-01-27 17:35:46 +0200

    homewerk

---
+++ Radio/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio radio = new Radio();
+
+            Console.WriteLine("Power: " + radio.Power);
+            radio.Power = true;
+            Console.WriteLine("Power: " + radio.Power);
+
+            Console.WriteLine("Vol: " + radio.Volume);
+            radio.Volume = 9;
+            Console.WriteLine("Vol: " + radio.Volume);
+
+            Console.WriteLine("Freq: " + radio.Frequency);
+            radio.Frequency = 26000.1;
+            Console.WriteLine("Freq: " + radio.Frequency);
+
+        }
+    }
+}
---
+++ Radio/Radio.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radio
+{
+    class Radio
+    {
+        private readonly int MIN_VOL = 0, MAX_VOL = 9;
+        private readonly double MIN_FREQ = 2000.0, MAX_FREQ = 26000.0;
+        private bool power;
+        private int volume;
+        private double frequency;
+
+        public Radio()
+        {
+
+        }
+
+        public bool Power
+        {
+            get { return power; }
+            set {
+                power = value;
+            }
+        }
+
+        public int Volume
+        {
+            get { return volume; }
+            set {
+                if (value > MAX_VOL)
+                {
+                    volume = MAX_VOL;
+                } else if (value < MIN_VOL)
+                {
+                    volume = MIN_VOL;
+                } else
+                {
+                    volume = value;
+                }
+            }
+        }
+
+        public double Frequency
+        {
+            get { return frequency; }
+            set
+            {
+                if (value > MAX_FREQ)
+                {
+                    frequency = MAX_FREQ;
+                }
+                else if (value < MIN_FREQ)
+                {
+                    frequency = MIN_FREQ;
+                }
+                else
+                {
+                    frequency = value;
+                }
+            }
+        }
+
+    }
+}
