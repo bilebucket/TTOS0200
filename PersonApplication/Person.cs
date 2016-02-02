20da988d27d89a06bebe9bf67460a6005cd569d2
sapake <codinghobo@gmail.com>
2016-02-02 16:58:13 +0200

    x

---
+++ PersonApplication/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApplication
+{
+    class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public int age { get; set; }
+        public string phoneNumber { get; set; }
+
+
+        public Person(string fn, string ln)
+        {
+            firstName = fn;
+            lastName = ln;
+        }
+
+        public void PersonMethod()
+        {
+            Console.WriteLine("persons method! wow so advanced!");
+        }
+
+        public override string ToString()
+        {
+            return firstName + " " + lastName + " " + address + " " + age + " " + phoneNumber;
+        }
+    }
+}
---
+++ PersonApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person s = new Person("John", "Doe");
+            s.PersonMethod();
+            Console.WriteLine(s.ToString());
+        }
+    }
+}
